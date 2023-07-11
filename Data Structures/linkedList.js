4537e9f786f803a238a6c09afb2d784535be250f
Yash Choudhary <ychoudhary9602@gmail.com>
2023-07-11 23:15:00 +0530

    linked list added

---
+++ Data Structures/linkedList.js
@@ -0,0 +1,171 @@
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+class linkedList {
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+
+    isEmpty() {
+        return this.size === 0
+    }
+
+    getSize() {
+        return this.size
+    }
+
+    // O(1 )
+    prepend(value) {
+        const node = new Node(value)
+        if (this.isEmpty()) {
+            this.head = node
+        } else {
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+    }
+
+    // O(n)
+    append(value) {
+        const node = new Node(value)
+        if (this.isEmpty()) {
+            this.head = node
+        } else {
+            let prev = this.head
+            while (prev.next) {
+                prev = prev.next
+            }
+            prev.next = node
+        }
+        this.size++
+    }
+
+    insert(value, index) {
+        if (index < 0 || index > this.size) {
+            return
+        }
+        if (index === 0) {
+            this.prepend(value)
+        } else {
+            const node = new Node(value)
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next = node
+            this.size++
+        }
+    }
+
+    removeFrom(index) {
+        if (index < 0 || index >= this.size) {
+            return null
+        }
+        let removedNode
+        if (index === 0) {
+            removedNode = this.head
+            this.head = this.head.next
+        } else {
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            removedNode = prev.next
+            prev.next = removedNode.next
+        }
+        this.size--
+        return removedNode.value
+    }
+
+    removeValue(value) {
+        if (this.isEmpty()) {
+            return null
+        }
+        if (this.head.value === value) {
+            this.head = this.head.next
+            this.size--
+            return value
+        } else {
+            let prev = this.head
+            while (prev.next && prev.next.value !== value) {
+                prev = prev.next
+            }
+            if (prev.next) {
+                const removedNode = prev.next
+                prev.next = removedNode.next
+                this.size--
+                return value
+            }
+            return null
+        }
+    }
+
+    search(value) {
+        if (this.isEmpty()) {
+            return -1
+        }
+        let i = 0
+        let curr = this.head
+        while (curr) {
+            if (curr.value === value) {
+                return i
+            }
+            curr = curr.next
+            i++
+        }
+        return -1
+    }
+
+    reverse() {
+        let prev = null
+        let curr = this.head
+        while (curr) {
+            let next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+        this.head = prev
+    }
+
+    print() {
+        if (this.isEmpty()) {
+            console.log('List is empty')
+        } else {
+            let curr = this.head
+            let listVlaues = ''
+            while (curr) {
+                listVlaues += `${curr.value} `
+                curr = curr.next
+            }
+            console.log(listVlaues)
+        }
+    }
+}
+
+const list = new linkedList()
+console.log('List is empty?', list.isEmpty())
+console.log('List size', list.getSize())
+list.print()
+
+list.insert(10, 0)
+list.print()
+
+list.insert(20, 0)
+list.print()
+
+list.insert(30, 0)
+list.print()
+
+list.insert(40, 0)
+list.print()
+
+list.reverse()
+list.print()
